10b3af77f63d166c8ddc1f5427e146627fe07601
DerekFei <dukefitch@hotmail.com>
2017-03-29 23:16:34 -0400

    add reducer and start redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,2 +1,11 @@
 var redux = require('redux');
 console.log('starting redux example');
+
+var reducer = (state={name:'Anonymous'}, action)=>{  //ES6 of state is null, using the default value
+  //state = state || {name: 'Anoymous'};
+  return state;
+};
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
